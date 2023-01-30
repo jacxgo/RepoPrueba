81e13d7ba2ffaf763740be20e739c0a75ba15492
javier jac <jac@mail.com>
2023-01-30 07:13:12 -0500

    Agregar archivos de proyecto.

---
+++ Sistema.Ventas/Controllers/ServicioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistema.Ventas.Controllers
+{
+    public class ServicioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Sistema.Ventas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema.Ventas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
